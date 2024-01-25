ddc93c9e079375f4175e5364354d344e72cb5829
martinmativo <mativomartin.m@gmail.com>
2024-01-24 16:51:55 -0800

    completed tasks

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+[200~#!/usr/bin/python3
+
+
+        def square_matrix_simple(matrix=[]):
+            """Compute the square value of all integers of a matrix."""
+                return ([list(map(lambda x: x * x, row)) for row in matrix])
