cd5cb24fd09654bcb15511fb015fa5d9028469ce
martinmativo <mativomartin.m@gmail.com>
2024-02-07 17:19:58 -0800

    completed tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-"""Define a class square."""
+"""Define a class Square."""
 
 
 class Square:
     """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square.
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
         """
-    self.__size = size
+        self.__size = size
