1e0b3c948cd894632f9e4271676a44ca51e03e17
martinmativo <mativomartin.m@gmail.com>
2024-02-07 17:04:29 -0800

    first attempt

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initializing a new square.
+
+        Args:
+            size (int): The size on new square.
+        """
+
+    self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Readme file for the 0x06-python-classes module
