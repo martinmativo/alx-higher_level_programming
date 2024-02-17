cf1e036041def44f30cbae703dd6c6fba4ad9d24
martinmativo <mativomartin.m@gmail.com>
2024-02-16 16:28:12 -0800

    completed fixes

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,21 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
-"""Prints a square."""
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """Defines a function to print a square with #character.
-    Args:
-        size (int): The size in length of the square
+    """Print a square with the # character.
 
+    Args:
+        size (int): The height/width of the square.
     Raises:
-        ValueError: If size is less than 0
-        TypeError: If size is a float and is less than 0
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
-    if size < 0:
-        raise ValueError("size must be >= 0")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         [print("#", end="") for j in range(size)]
