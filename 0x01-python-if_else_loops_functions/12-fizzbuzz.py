b196b399f514cf0d841b4d7483c871c44bc0b776
martinmativo <mativomartin.m@gmail.com>
2024-03-13 08:39:13 -0700

    removed unused ested function in task 12 and completed optional tasks 14 and task 15

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,18 +3,17 @@
 
 def fizzbuzz():
     """
-    Function that prints the numbers from 1 to 100 separated by a space.
-    For multiples of three print Fizz.
-    For multiples of five print Buzz.
-    For numbers which are multiples of both three and five print FizzBuzz.
+    Print the numbers from 1 to 100 separated by a space.
+    For multiples of three, print Fizz instead of the number.
+    For multiples of five, print Buzz instead of the number.
+    For multiples of three and five, print FizzBuzz instead of the number.
     """
-    def fizzbuzz():
-        for number in range (0, 101):
-            if number % 3 == 0:
-                print("Fizz ", end="")
-            elif number % 5 == 0:
-                print("Buzz ", end="")
-            elif number % 3 == 0 and number % 5 == 0:
-                print("FizzBuzz ",end="")
-            else:
-                print(f"{number} ",end="")
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print(f"{number} ", end="")
